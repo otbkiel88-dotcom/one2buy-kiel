78c53a1b3268ca05c5ad8c082a3a93258ee5b0ba
otbkiel88-dotcom <otbkiel88@gmail.com>
2026-06-08 21:38:54 +0200

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -44,5 +44,6 @@ export const SITE = {
     { label: 'Standort', href: '/standorte' },
     { label: 'Über uns', href: '/ueber-uns' },
     { label: 'Kontakt', href: '/kontakt' },
+    { label: 'FAQ', href: '/faq' },
   ],
 };
